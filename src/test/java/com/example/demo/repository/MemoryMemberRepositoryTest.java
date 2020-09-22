b2278ec7695a45849b34ce71dad8934e69fff499
sangwonchoi <sangwonchoi@sangwonchoiui-MacBookPro.local>
2020-09-22 14:15:06 +0900

    Initial commit

---
+++ src/test/java/com/example/demo/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    // AfterEach testcase method 하나를 실행하고 메모리를 비운다. (공용 데이터를 비운다.)
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertEquals(result, member);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertEquals(result, member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertEquals(result.size(), 2);
+    }
+}
